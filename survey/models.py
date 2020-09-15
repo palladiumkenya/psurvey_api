1b051b4badc16d9e8575f767a1031a600dd26592
cbrianbet <cbrian@mhealthkenya.org>
2020-09-15 17:12:08 +0000

    Server changes

--- survey/models.py
+++ survey/models.py
@@ -18,7 +18,7 @@ class Questionnaire (models.Model):
 
 class Question (models.Model):
     questionnaire = models.ForeignKey(Questionnaire, on_delete=models.CASCADE)
-    question =  models.CharField(max_length=50)
+    question =  models.CharField(max_length=150)
     question_type = models.IntegerField()
     created_at = models.DateTimeField(auto_now=True)
     created_by = models.ForeignKey(Users, on_delete=models.CASCADE)
@@ -41,7 +41,7 @@ class Started_Questionnaire (models.Model):
     ccc_number = models.CharField(max_length=15)
     firstname = models.CharField(max_length=30)
     questionnaire = models.ForeignKey(Questionnaire, on_delete=models.CASCADE)
-    started_by = models.ForeignKey(Users, on_delete=models.CASCADE)
+    started_by = models.ForeignKey(Users, on_delete=models.CASCADE, default=1)
 
     class Meta:
         db_table = "Started_Questionnaire"
@@ -51,7 +51,7 @@ class Response (models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
     open_text = models.CharField(max_length=150, blank=True, null=True)
-    session = models.ForeignKey(Started_Questionnaire, on_delete=models.CASCADE)
+    session = models.ForeignKey(Started_Questionnaire, on_delete=models.CASCADE, default=1)
 
     class Meta:
         db_table = "Responses"
