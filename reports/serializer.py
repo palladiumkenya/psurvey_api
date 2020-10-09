bbb4cf37f2a366ec84ca85ac7492777bc52ef298
cbrianbet <cbrianbet@outlook.com>
2020-10-09 12:52:15 +0300

    Pateint list complete

--- reports/serializer.py
+++ reports/serializer.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 
 from authApp.serializer import *
-from survey.models import Response, Answer, End_Questionnaire, Started_Questionnaire
+from survey.models import Response, Answer, End_Questionnaire, Started_Questionnaire, Questionnaire, Question
+from survey.serializer import QuestionSerializer, QuestionnaireSerializer
 
 
 class AnswerSerializer(serializers.ModelSerializer):
@@ -16,6 +17,15 @@ class RespSerializer(serializers.ModelSerializer):
     class Meta:
         model = Response
         fields = '__all__'
+
+    def to_representation(self, value):
+        data = super().to_representation(value)
+        ques = Question.objects.get(id=data['question'])
+        quesnn = Questionnaire.objects.get(id=ques.questionnaire.id)
+        data.update({'question': QuestionSerializer(ques).data,
+                     'questionnaire': QuestionnaireSerializer(quesnn).data})
+
+        return data
 
 
 class AllUserSerializer(serializers.ModelSerializer):
@@ -47,7 +57,8 @@ class PatientSer(serializers.ModelSerializer):
     def to_representation(self, value):
         data = super().to_representation(value)
         resp = Response.objects.filter(session=data['id']).order_by('-created_at')
-
-        data.update({'responses': ResponsesSerilizer(resp, many=True).data})
+        print(Questionnaire.objects.get(id=data['questionnaire']).name)
+        data.update({'responses': RespSerializer(resp, many=True).data,
+                     'questionnaire': Questionnaire.objects.get(id=data['questionnaire']).name})
 
         return data
--- reports/urls.py
+++ reports/urls.py
@@ -9,7 +9,7 @@ from django.urls import path, include
 router = routers.DefaultRouter()
 router.register(r'albums/(?P<question_id>\d+)', views.RespViewSet, basename='Response')
 router.register(r'reports/list', views.Current_user, basename='Users')
-router.register(r'patients/list', views.Patients, basename='Start_Questionnaire')
+# router.register(r'patients/list', views.Patients, basename='Start_Questionnaire')
 
 urlpatterns = [
     #web urls
@@ -17,6 +17,7 @@ urlpatterns = [
     path('web/reports/open_resp/<int:q_id>', views.open_end, name='open_resp_report'),
     path('web/reports/users', views.users_report, name='users_report'),
     path('web/reports/patients', views.patients_report, name='patients_report'),
+    path('web/patients/list', views.Patients, name='patients_list'),
     url('^api/', include(router.urls)),
 
 ]
